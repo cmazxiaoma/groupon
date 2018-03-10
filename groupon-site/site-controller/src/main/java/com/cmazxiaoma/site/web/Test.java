d9d57b9cbcdf3f6d533f72fe90dfe3f6ca72b91d
cmazxiaoma <1363739582@qq.com>
2018-03-10 17:08:35 +0800

    解决groupon-services模块下pom.xml不能生成jar包的问题

---
+++ groupon-site/site-controller/src/main/java/com/cmazxiaoma/site/web/Test.java
@@ -0,0 +1,15 @@
+package com.cmazxiaoma.site.web;
+
+import lombok.Data;
+
+/**
+ * @author cmazxiaoma
+ * @version V1.0
+ * @Description: TODO
+ * @date 2018/3/10 15:30
+ */
+@Data
+public class Test {
+
+    private String id;
+}
